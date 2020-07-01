eb0c3a2b46b1c1ae73651253b7841402d71af9e1
monstaro <codysmith2814@gmail.com>
2020-06-30 22:36:11 -0600

    Removes unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
-import Home from "./Components/Home";
+import { Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import HeaderContainer from "./Components/HeaderContainer/HeaderContainer";
 import Merch from "./Components/Merch/Merch";
 import Music from "./Components/Music/Music";
 import Pics from "./Components/Pics/Pics";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-    <HeaderContainer />
-    <Route path="/" exact component={Home} />
-    <Route path="/about" exact component={About} />
-    <Route path="/merch" exact component={Merch} />
-    <Route path="/music" exact component={Music} />
-    <Route path="/pics" exact component={Pics} />
-    <Route path="/contact" exact component={Contact} />
+      <HeaderContainer />
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      <Route path="/merch" exact component={Merch} />
+      <Route path="/music" exact component={Music} />
+      <Route path="/pics" exact component={Pics} />
+      <Route path="/contact" exact component={Contact} />
+      <Footer />
     </div>
   );
 }
