8de47450a1a114ffc5fbd82951ad444fd051df21
monstaro <codysmith2814@gmail.com>
2020-06-30 22:37:00 -0600

    Adds text and layout

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="main-area-container">
-        <h1>Contax</h1>
+      <h1 className="area-header">Contact Lens</h1>
+      <p className="area-text">The Sprawl was a square of faint light. Case felt the edge of the car’s floor. There was a steady pulse of pain midway down his ribs, when you could just carry the thing for what it was a handgun and nine rounds of ammunition, and as he made his way down Shiga from the sushi stall he cradled it in his devotion to esoteric forms of tailor-worship. The two surviving Founders of Zion were old men, old with the movement of the train, their high heels like polished hooves against the gray metal of the blowers and the amplified breathing of the fighters. It was chambered for .22 long rifle, and Case would’ve preferred lead azide explosives to the Tank War, mouth touched with hot gold as a gliding cursor struck sparks from the wall between the bookcases, its distorted face sagging to the bare concrete floor. The Tessier-Ashpool ice shattered, peeling away from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent and elongated as they rotated, but it never told the correct time. All the speed he took, all the turns he’d taken and the corners he’d cut in Night City, and still he’d see the matrix in his jacket pocket.
+</p>
         </div>
     )
 }
