f6593f9dd63159352e6b6469abc107b3579fc4ff
monstaro <codysmith2814@gmail.com>
2020-06-30 22:36:47 -0600

    Adds text and layout

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './About.scss'
+import React from "react";
+import "./About.scss";
+import hc from "../../icons/hc1.jpg";
+
 const About = () => {
-    return (
-        <div className="main-area-container">
-<h1>ABout</h1>
-        </div>
-    )
-}
+  return (
+    <div className="main-area-container">
+      <div className="image-area">
+      <img className="about-img" src={hc} alt="the boys at the beach" />
+      Alt Rock from Denver, CO
+      <div className="members">
+        <p className="area-text">Rob Knox - Drums</p>
+        <p className="area-text">Justin Jokinen - Guitar</p>
+        <p className="area-text">Cody Smith - Guitar, Vox</p>
+        <p className="area-text">Frank McGinn - Bass</p>
+      </div>
+      </div>
+      <button className="epk">View Full EPK (.pdf)</button>
+    </div>
+  );
+};
 
 export default About;
