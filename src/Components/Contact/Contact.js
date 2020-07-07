3bbe80b3d6209cf384dd3c46e4ebff12993da779
monstaro <codysmith2814@gmail.com>
2020-07-06 20:56:56 -0600

    Adds form component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,13 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Contact.scss";
 
 const Contact = () => {
-    return (
-        <div className="main-area-container">
-      <h1 className="area-header">Contact Lens</h1>
-      <p className="area-text">The Sprawl was a square of faint light. Case felt the edge of the car’s floor. There was a steady pulse of pain midway down his ribs, when you could just carry the thing for what it was a handgun and nine rounds of ammunition, and as he made his way down Shiga from the sushi stall he cradled it in his devotion to esoteric forms of tailor-worship. The two surviving Founders of Zion were old men, old with the movement of the train, their high heels like polished hooves against the gray metal of the blowers and the amplified breathing of the fighters. It was chambered for .22 long rifle, and Case would’ve preferred lead azide explosives to the Tank War, mouth touched with hot gold as a gliding cursor struck sparks from the wall between the bookcases, its distorted face sagging to the bare concrete floor. The Tessier-Ashpool ice shattered, peeling away from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent and elongated as they rotated, but it never told the correct time. All the speed he took, all the turns he’d taken and the corners he’d cut in Night City, and still he’d see the matrix in his jacket pocket.
-</p>
-        </div>
-    )
-}
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [errorMsg, setError] = useState(null);
+
+  function onNameChange(event) {
+    setName({ name: event.target.value });
+  }
+
+  function onEmailChange(event) {
+    setEmail({ email: event.target.value });
+  }
+
+  function onMessageChange(event) {
+    setMessage({ message: event.target.value });
+  }
+
+  // const handleSubmit = (event) => {
+  //     console.log(event)
+  // }
+  const errorHandler = (e) => {
+    e.preventDefault();
+    console.log(email)
+    if ((!name || !email || !message) || (!email.email || !name.name || !message.message)) {
+      setError("Please fill out one of the missing fields.")
+    } else {
+        sendFeedback('template_cpfgiItB', {message_html: message.message, from_name: name.name, reply_to: email.email})
+        setError(null)
+    }
+  };
+
+  const sendFeedback = (templateId, vars) => {
+    window.emailjs.send(
+        'gmail', templateId, vars
+    ).then(res => {
+        console.log('Sent it')
+    })
+    .catch(err => console.error(err))
+  }
+  return (
+    <div className="main-area-container">
+      <h1 className="area-header">Contact</h1>
+      <p className="area-text">
+        <form className="contact-form">
+          <div className="input-section">
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              onChange={(event) => onNameChange(event)}
+              className="text-input"
+            ></input>
+          </div>
+          <div className="input-section">
+            <input
+              type="text"
+              placeholder="Enter Your Email"
+              onChange={(event) => onEmailChange(event)}
+              className="text-input"
+            ></input>
+          </div>
+          <div className="input-section">
+            <input
+              type="text"
+              placeholder="Enter Your Message"
+              onChange={(event) => onMessageChange(event)}
+              className="text-input tall"
+            ></input>
+          </div>
+          <input
+            type="submit"
+            onClick={(e) => errorHandler(e)}
+            className="submitBtn"
+            value="submit"
+          ></input>
+        </form>
+      </p>
+      <p className="alert-text hide">{errorMsg}</p>
+    </div>
+  );
+};
 
 export default Contact;
