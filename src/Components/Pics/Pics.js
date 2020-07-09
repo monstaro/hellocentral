93a42edefc7bce923c0bede61c69f973b1a73af1
monstaro <codysmith2814@gmail.com>
2020-07-08 20:40:19 -0600

    Begins pic section

--- src/Components/Pics/Pics.js
+++ src/Components/Pics/Pics.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Pics = () => {
-    return (
-        <div className="main-area-container">
+  return (
+    <div className="main-area-container">
       <h1 className="area-header">Pixx</h1>
-      <p className="area-text">He stared at the clinic, Molly took him to the simple Chinese hollow points Shin had sold him. Images formed and reformed: a flickering montage of the Sprawl’s towers and ragged Fuller domes, dim figures moving toward him in the Japanese night like live wire voodoo and he’d cry for it, cry in his devotion to esoteric forms of tailor-worship. That was Wintermute, manipulating the lock the way it had manipulated the drone micro and the corners he’d cut in Night City, and still he’d see the matrix in his sleep, and wake alone in the coffin for Armitage’s call. Its hands were holograms that altered to match the convolutions of the car’s floor. Its hands were holograms that altered to match the convolutions of the deck sting his palm as he made his way down Shiga from the sushi stall he cradled it in his jacket pocket. Case had never seen him wear the same suit twice, although his wardrobe seemed to consist entirely of meticulous reconstruction’s of garments of the spherical chamber. Now this quiet courtyard, Sunday afternoon, this girl with a ritual lack of urgency through the center of his closed left eyelid. He woke and found her stretched beside him in the center of his closed left eyelid.
-</p>
-        </div>
-    )
-}
+      <p className="area-text">
+        <a
+          data-flickr-embed="true"
+          href="https://www.flickr.com/photos/189208467@N07/albums/72157715000772437"
+          title="hellocentral"
+        >
+          <img
+            src="https://live.staticflickr.com/65535/50085703107_d30a1081b8.jpg"
+            width="500"
+            height="500"
+            alt="hellocentral"
+          />
+        </a>
+        <script
+          async
+          src="//embedr.flickr.com/assets/client-code.js"
+          charset="utf-8"
+        ></script>
+      </p>
+    </div>
+  );
+};
 
 export default Pics;
