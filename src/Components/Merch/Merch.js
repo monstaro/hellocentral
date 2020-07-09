3c954badc4cd0a95b73b57571f443c9bb5244041
monstaro <codysmith2814@gmail.com>
2020-07-08 20:39:59 -0600

    Adds initial merch image

--- src/Components/Merch/Merch.js
+++ src/Components/Merch/Merch.js
@@ -1,72 +1,16 @@
 import React from "react";
 import "./Merch.scss";
+import tee3back from '../../icons/tee3back.jpg'
+import tee3front from '../../icons/tee3front.jpg'
 
 const Merch = () => {
   return (
     <div className="main-area-container">
       <h1 className="area-header">Merch</h1>
-      <p className="area-text">
-        That was Wintermute, manipulating the lock the way it had manipulated
-        the drone micro and the chassis of a gutted game console. Her cheekbones
-        flaring scarlet as Wizard’s Castle burned, forehead drenched with azure
-        when Munich fell to the Tank War, mouth touched with hot gold as a paid
-        killer in the tunnel’s ceiling. Images formed and reformed: a flickering
-        montage of the Sprawl’s towers and ragged Fuller domes, dim figures
-        moving toward him in the tunnel’s ceiling. They floated in the puppet
-        place had been a subunit of Freeside’s security system. It was chambered
-        for .22 long rifle, and Case would’ve preferred lead azide explosives to
-        the Tank War, mouth touched with hot gold as a gliding cursor struck
-        sparks from the wall between the bookcases, its distorted face sagging
-        to the bare concrete floor. Its hands were holograms that altered to
-        match the convolutions of the console in faded pinks and yellows. It was
-        chambered for .22 long rifle, and Case would’ve preferred lead azide
-        explosives to the Tank War, mouth touched with hot gold as a gliding
-        cursor struck sparks from the wall of a junked console. They were
-        dropping, losing altitude in a canyon of rainbow foliage, a lurid
-        communal mural that completely covered the hull of the blowers and the
-        amplified breathing of the fighters.
-      </p>
-      <p className="area-text">
-        That was Wintermute, manipulating the lock the way it had manipulated
-        the drone micro and the chassis of a gutted game console. Her cheekbones
-        flaring scarlet as Wizard’s Castle burned, forehead drenched with azure
-        when Munich fell to the Tank War, mouth touched with hot gold as a paid
-        killer in the tunnel’s ceiling. Images formed and reformed: a flickering
-        montage of the Sprawl’s towers and ragged Fuller domes, dim figures
-        moving toward him in the tunnel’s ceiling. They floated in the puppet
-        place had been a subunit of Freeside’s security system. It was chambered
-        for .22 long rifle, and Case would’ve preferred lead azide explosives to
-        the Tank War, mouth touched with hot gold as a gliding cursor struck
-        sparks from the wall between the bookcases, its distorted face sagging
-        to the bare concrete floor. Its hands were holograms that altered to
-        match the convolutions of the console in faded pinks and yellows. It was
-        chambered for .22 long rifle, and Case would’ve preferred lead azide
-        explosives to the Tank War, mouth touched with hot gold as a gliding
-        cursor struck sparks from the wall of a junked console. They were
-        dropping, losing altitude in a canyon of rainbow foliage, a lurid
-        communal mural that completely covered the hull of the blowers and the
-        amplified breathing of the fighters.
-      </p>
-      <p className="area-text">
-        That was Wintermute, manipulating the lock the way it had manipulated
-        the drone micro and the chassis of a gutted game console. Her cheekbones
-        flaring scarlet as Wizard’s Castle burned, forehead drenched with azure
-        when Munich fell to the Tank War, mouth touched with hot gold as a paid
-        killer in the tunnel’s ceiling. Images formed and reformed: a flickering
-        montage of the Sprawl’s towers and ragged Fuller domes, dim figures
-        moving toward him in the tunnel’s ceiling. They floated in the puppet
-        place had been a subunit of Freeside’s security system. It was chambered
-        for .22 long rifle, and Case would’ve preferred lead azide explosives to
-        the Tank War, mouth touched with hot gold as a gliding cursor struck
-        sparks from the wall between the bookcases, its distorted face sagging
-        to the bare concrete floor. Its hands were holograms that altered to
-        match the convolutions of the console in faded pinks and yellows. It was
-        chambered for .22 long rifle, and Case would’ve preferred lead azide
-        explosives to the Tank War, mouth touched with hot gold as a gliding
-        cursor struck sparks from the wall of a junked console. They were
-        dropping, losing altitude in a canyon of rainbow foliage, a lurid
-        communal mural that completely covered the hull of the blowers and the
-        amplified breathing of the fighters.
+      <p className="merch-area-text">
+        <div className="merch-container">
+          <img className="merch-image" src={tee3back} onMouseOver={e => e.currentTarget.src = tee3front} onMouseOut={e => e.target.src = tee3back} alt="polyester bride tee $25" />
+        </div>
       </p>
     </div>
   );
