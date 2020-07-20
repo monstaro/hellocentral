261b562d098e18d7d0a08b0dac335c8bc0d539f9
monstaro <codysmith2814@gmail.com>
2020-07-20 16:26:59 -0600

    Imports react boostrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Router } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import HeaderContainer from "./Components/HeaderContainer/HeaderContainer";
@@ -8,18 +8,20 @@ import Music from "./Components/Music/Music";
 import Pics from "./Components/Pics/Pics";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="app">
       <HeaderContainer />
+      {/* <Router basename='/' > */}
       <Route path="/about" exact component={About} />
       <Route path="/merch" exact component={Merch} />
       <Route path="/music" exact component={Music} />
       <Route path="/pics" exact component={Pics} />
       <Route path="/contact" exact component={Contact} />
-      <Route path="" component={Home} />
+      <Route path="/"  exact component={Home} />
+      {/* </Router> */}
       <Footer />
     </div>
   );
