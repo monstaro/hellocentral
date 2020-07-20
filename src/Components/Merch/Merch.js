89a1b213ee89dd643acf82fc1bb82edf1e016e86
monstaro <codysmith2814@gmail.com>
2020-07-20 16:27:49 -0600

    Adds merch images

--- src/Components/Merch/Merch.js
+++ src/Components/Merch/Merch.js
@@ -1,15 +1,61 @@
 import React from "react";
 import "./Merch.scss";
-import tee3back from '../../icons/tee3back.jpg'
-import tee3front from '../../icons/tee3front.jpg'
+import tee1 from "../../icons/1.png";
+import tee1back from "../../icons/1back.png";
+import tee2 from "../../icons/2.png";
+import tee3 from "../../icons/3.png";
+import tee3back from "../../icons/3back.png";
+import tee4 from "../../icons/4.png";
+import tee5 from "../../icons/hoodie.png";
 
 const Merch = () => {
   return (
     <div className="main-area-container">
       <h1 className="area-header">Merch</h1>
       <p className="merch-area-text">
+      <div className="merch-container">
+          <img
+            className="merch-image"
+            src={tee5}
+            alt="polyester bride tee $25"
+          />
+          <p>'93 Hoodie - $23</p>
+        </div>
         <div className="merch-container">
-          <img className="merch-image" src={tee3back} onMouseOver={e => e.currentTarget.src = tee3front} onMouseOut={e => e.target.src = tee3back} alt="polyester bride tee $25" />
+          <img
+            className="merch-image"
+            src={tee1back}
+            onMouseOver={(e) => (e.currentTarget.src = tee1)}
+            onMouseOut={(e) => (e.target.src = tee1back)}
+            alt="polyester bride tee $25"
+          />
+          <p>Classic Tee White - $18</p>
+        </div>
+        <div className="merch-container">
+          <img
+            className="merch-image"
+            src={tee3back}
+            onMouseOver={(e) => (e.currentTarget.src = tee3)}
+            onMouseOut={(e) => (e.target.src = tee3back)}
+            alt="polyester bride tee $25"
+          />
+          <p>Classic Tee Black - $18</p>
+        </div>
+        <div className="merch-container">
+          <img
+            className="merch-image"
+            src={tee2}
+            alt="polyester bride tee $25"
+          />
+          <p>Telephone Tee - $18</p>
+        </div>
+        <div className="merch-container">
+          <img
+            className="merch-image"
+            src={tee4}
+            alt="polyester bride tee $25"
+          />
+          <p>Rose Tee - $18</p>
         </div>
       </p>
     </div>
