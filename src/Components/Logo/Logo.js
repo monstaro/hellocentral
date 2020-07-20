0d749459fe4b630cedd7c9f2b8c3bb2b4989b216
monstaro <codysmith2814@gmail.com>
2020-07-20 16:30:03 -0600

    Adds hover effect on text

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import '../../fonts/Linebeam.ttf';
-import './Logo.scss';
+import React from "react";
+import "../../fonts/Linebeam.ttf";
+import "./Logo.scss";
 
 const Logo = () => {
-    return (
-        <div className="logo">
-<p className="name">hellocentral</p>
-        </div>
-    )
-}
+  return (
+    <div className="logo">
+      <p className="name">
+        <span class="name-hover">h</span>
+        <span class="name-hover">e</span>
+        <span class="name-hover">l</span>
+        <span class="name-hover">l</span>
+        <span class="name-hover">o</span>
+        <span class="name-hover">c</span>
+        <span class="name-hover">e</span>
+        <span class="name-hover">n</span>
+        <span class="name-hover">t</span>
+        <span class="name-hover">r</span>
+        <span class="name-hover">a</span>
+        <span class="name-hover">l</span>
+      </p>
+    </div>
+  );
+};
 
 export default Logo;
