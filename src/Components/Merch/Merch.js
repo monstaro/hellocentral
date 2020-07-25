db1dc3839915855c8b29c6e8ba9750de3fdea5cd
monstaro <codysmith2814@gmail.com>
2020-07-25 12:57:23 -0600

    Changes tee text

--- src/Components/Merch/Merch.js
+++ src/Components/Merch/Merch.js
@@ -7,6 +7,7 @@ import tee3 from "../../icons/3.png";
 import tee3back from "../../icons/3back.png";
 import tee4 from "../../icons/4.png";
 import tee5 from "../../icons/hoodie.png";
+import tee6 from "../../icons/565.png";
 
 const Merch = () => {
   return (
@@ -14,14 +15,17 @@ const Merch = () => {
       <h1 className="area-header">Merch</h1>
       <p className="merch-area-text">
       <div className="merch-container">
+        <a href="https://bit.ly/2BlKlYv" rel="noopener noreferrer" target="_blank">
           <img
             className="merch-image"
             src={tee5}
             alt="polyester bride tee $25"
           />
           <p>'93 Hoodie - $23</p>
+          </a>
         </div>
         <div className="merch-container">
+          <a href="https://bit.ly/30ECv52" rel="noopener noreferrer" target="_blank">
           <img
             className="merch-image"
             src={tee1back}
@@ -29,9 +33,12 @@ const Merch = () => {
             onMouseOut={(e) => (e.target.src = tee1back)}
             alt="polyester bride tee $25"
           />
-          <p>Classic Tee White - $18</p>
+          <p>Classic Tee White - $18</p>        
+          </a>
         </div>
         <div className="merch-container">
+          <a href="https://bit.ly/2ZPCegc" rel="noopener noreferrer" target="_blank">
+
           <img
             className="merch-image"
             src={tee3back}
@@ -40,22 +47,38 @@ const Merch = () => {
             alt="polyester bride tee $25"
           />
           <p>Classic Tee Black - $18</p>
+                      
+          </a>
         </div>
         <div className="merch-container">
+          <a href="https://bit.ly/2ZTZppY" rel="noopener noreferrer" target="_blank">
           <img
             className="merch-image"
             src={tee2}
             alt="polyester bride tee $25"
           />
           <p>Telephone Tee - $18</p>
+          </a>
         </div>
         <div className="merch-container">
+          <a href="https://bit.ly/3hsOwkN" rel="noopener noreferrer" target="_blank">
           <img
             className="merch-image"
             src={tee4}
             alt="polyester bride tee $25"
           />
           <p>Rose Tee - $18</p>
+          </a>
+        </div>
+        <div className="merch-container">
+          <a href="https://teespring.com/basic-tee-5850?tsmac=store&tsmic=hellocentral-2&pid=46&cid=2739" rel="noopener noreferrer" target="_blank">
+          <img
+            className="merch-image"
+            src={tee6}
+            alt="polyester bride tee $25"
+          />
+          <p>Basic Tee - $18</p>
+          </a>
         </div>
       </p>
     </div>
