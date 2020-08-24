ecd6a76dc9732f8dfcf63e52b992c1d9841e3700
monstaro <codysmith2814@gmail.com>
2020-08-24 09:43:38 -0600

    Updates homepage text

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,12 +4,12 @@ const Home = () => {
   return (
     <div>
       <div className="main-area-container">
-        <h1 className="home-area-header">HEATWAVE AUGUST 21</h1>
-        <h2 className="home-area-header">HEATWAVE AUGUST 21</h2>
-        <h3 className="home-area-header">HEATWAVE AUGUST 21</h3>
-        <h4 className="home-area-header">HEATWAVE AUGUST 21</h4>
-        <h5 className="home-area-header">HEATWAVE AUGUST 21</h5>
-        <h6 className="home-area-header">HEATWAVE AUGUST 21</h6>
+        <h1 className="home-area-header">HEATWAVE OUT NOW</h1>
+        <h2 className="home-area-header">HEATWAVE OUT NOW</h2>
+        <h3 className="home-area-header">HEATWAVE OUT NOW</h3>
+        <h4 className="home-area-header">HEATWAVE OUT NOW</h4>
+        <h5 className="home-area-header">HEATWAVE OUT NOW</h5>
+        <h6 className="home-area-header">HEATWAVE OUT NOW</h6>
       </div>
     </div>
   );
