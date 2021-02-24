048c0abed5cccf94bc490ff0e120d6b705b7c101
monstaro <codysmith2814@gmail.com>
2021-02-24 16:08:14 -0700

    Init commit - inner box

---
+++ src/Components/SiteContainer/SiteContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SiteContainer.scss';
+import HeaderContainer from '../HeaderContainer/HeaderContainer';
+export const SiteContainer = () => {
+    return (
+    <div className="site-container">
+        {/* hi */}
+        <HeaderContainer />
+    </div>
+    )
+}
+
+export default SiteContainer;
