6f0e431eaaf5e0fcb7c9baf73e20c61d9824ed7b
monstaro <codysmith2814@gmail.com>
2021-02-24 16:56:45 -0700

    Finish colour function

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -2,12 +2,11 @@ import React from "react";
 import "../../fonts/Linebeam.ttf";
 import "../../fonts/HelveticaNeue.ttf";
 import "./Logo.scss";
-import pinklogo from '../../images/logo-pink.png'
 
 const Logo = () => {
 
   const generateRandomColor = () => {
-    let colors = ['yellow', 'pink'];
+    let colors = ['yellow', 'pink', 'teal', 'white', 'green'];
     let randomNumber = Math.floor(Math.random() * Math.floor(colors.length))
     return `hellocentral ${colors[randomNumber]}`
   }
@@ -16,7 +15,6 @@ const Logo = () => {
     <div className="logo">
       <p className={generateRandomColor()}>
         hellocentral
-        {/* <img src={pinklogo} style={{width: '600px'}}/> */}
       </p>
     </div>
   );
