b8a14b18762cc07a54ff98009249c06bbe322cf2
monstaro <codysmith2814@gmail.com>
2021-02-24 16:06:28 -0700

    Add SiteContainer component for shaded box

--- src/App.js
+++ src/App.js
@@ -9,23 +9,26 @@ import Music from "./Components/Music/Music";
 import Pics from "./Components/Pics/Pics";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import SiteContainer from './Components/SiteContainer/SiteContainer';
 import "./App.scss";
 
 function App() {
   return (
     <div className="app">
+      <SiteContainer>
       <HeaderContainer />
-      <Switch>
+      {/* <Switch> */}
       {/* <HashRouter basename='/' > */}
-      <Route path="/about"  component={About} />
-      <Route path="/merch" exact component={Merch} />
-      <Route path="/music" exact component={Music} />
-      <Route path="/pics" exact component={Pics} />
-      <Route path="/contact" exact component={Contact} />
-      <Route path="/"  component={Home} />
+      {/* <Route path="/about"  component={About} /> */}
+      {/* <Route path="/merch" exact component={Merch} /> */}
+      {/* <Route path="/music" exact component={Music} /> */}
+      {/* <Route path="/pics" exact component={Pics} /> */}
+      {/* <Route path="/contact" exact component={Contact} /> */}
+      {/* <Route path="/"  component={Home} /> */}
       {/* </HashRouter> */}
-      </Switch>
-      <Footer />
+      {/* </Switch> */}
+      {/* <Footer /> */}
+      </SiteContainer>
     </div>
   );
 }
