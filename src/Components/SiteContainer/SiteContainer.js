60ad81fa04cac643014442460f61520c3a804923
monstaro <codysmith2814@gmail.com>
2021-02-24 21:21:14 -0700

    Bring in merch

--- src/Components/SiteContainer/SiteContainer.js
+++ src/Components/SiteContainer/SiteContainer.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './SiteContainer.scss';
-import HeaderContainer from '../HeaderContainer/HeaderContainer';
+import React from "react";
+import "./SiteContainer.scss";
+import HeaderContainer from "../HeaderContainer/HeaderContainer";
+import { Route, Switch } from "react-router-dom";
+import About from "../About/About";
+import Home from "../Home/Home";
+import Merch from "../Merch/Merch";
+
+import Footer from "../Footer/Footer";
+
 export const SiteContainer = () => {
-    return (
+  return (
     <div className="site-container">
-        {/* hi */}
-        <HeaderContainer />
+      <HeaderContainer />
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/merch" component={Merch} />
+
+        <Route path="/" component={Home} />
+      </Switch>
+     <Footer />
     </div>
-    )
-}
+  );
+};
 
 export default SiteContainer;
