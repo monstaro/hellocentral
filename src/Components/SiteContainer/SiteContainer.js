c691a0b2cd525b4be00a903e241024fc3fbb95dc
monstaro <codysmith2814@gmail.com>
2021-02-24 22:04:39 -0700

    Import rest of components

--- src/Components/SiteContainer/SiteContainer.js
+++ src/Components/SiteContainer/SiteContainer.js
@@ -4,9 +4,11 @@ import HeaderContainer from "../HeaderContainer/HeaderContainer";
 import { Route, Switch } from "react-router-dom";
 import About from "../About/About";
 import Home from "../Home/Home";
-import Merch from "../Merch/Merch";
-
+import Music from "../Music/Music";
 import Footer from "../Footer/Footer";
+import Merch from "../Merch/Merch";
+import Pics from "../Pics/Pics";
+import Contact from "../Contact/Contact";
 
 export const SiteContainer = () => {
   return (
@@ -15,7 +17,9 @@ export const SiteContainer = () => {
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/merch" component={Merch} />
-
+        <Route path="/music" component={Music} />
+        <Route path="/pics" component={Pics} />
+        <Route path="/contact" component={Contact} />
         <Route path="/" component={Home} />
       </Switch>
       <Footer />
