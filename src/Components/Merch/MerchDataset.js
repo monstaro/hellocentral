b91eff41d4210cd6f79b7b80784ffcec27465a6a
monstaro <codysmith2814@gmail.com>
2021-02-24 21:18:04 -0700

    Builds dataset for merch items

---
+++ src/Components/Merch/MerchDataset.js
@@ -0,0 +1,98 @@
+import hoodie93 from "./MerchPics/93hoodie.png";
+import palmhoodie from "./MerchPics/palmhoodie.png";
+import teletee from "./MerchPics/teletee.png";
+import ghostblackback from "./MerchPics/ghostblackback.png";
+import ghostblackfront from "./MerchPics/ghostblackfront.png";
+import ghostwhiteback from "./MerchPics/ghostwhiteback.png";
+import ghostwhitefront from "./MerchPics/ghostwhitefront.png";
+import rose from "./MerchPics/rose.png";
+import skullfront from './MerchPics/skullfront.png';
+import skullback from './MerchPics/skullback.png';
+import classicfront from './MerchPics/classicfront.png';
+import classicback from './MerchPics/classicback.png';
+import classicblkfront from './MerchPics/classicblkfront.png';
+import classicblkback from './MerchPics/classicblkback.png';
+import basic from './MerchPics/basic.png';
+import rosetank from './MerchPics/rosetank.png';
+
+const merchItems = [
+    {
+        name: '1993',
+        url: 'https://bit.ly/2BlKlYv',
+        frontImg: hoodie93,
+        backImg: null,
+        price: '$23',
+    },
+    {
+        name: 'Palm Canyon',
+        url: 'https://bit.ly/2YvWYZw',
+        frontImg: palmhoodie,
+        backImg: null,
+        price: '$23',
+    },
+    {
+        name: 'Operator',
+        url: 'https://bit.ly/2ZTZppY',
+        frontImg: teletee,
+        backImg: null,
+        price: '$18',
+    },
+    {
+        name: 'Ghost Palm (Blk, Organic)',
+        url: 'https://bit.ly/2FKOR4C',
+        frontImg: ghostblackfront,
+        backImg: ghostblackback,
+        price: '$24',
+    },
+    {
+        name: 'Ghost Palm (Wht, Organic)',
+        url: 'https://bit.ly/32pcrMd',
+        frontImg: ghostwhitefront,
+        backImg: ghostwhiteback,
+        price: '$24',
+    },
+    {
+        name: 'Rose',
+        url: 'https://bit.ly/3dKyGTO',
+        frontImg: rose,
+        backImg: null,
+        price: '$18',
+    },
+    {
+        name: 'Skull (Organic)',
+        url: 'https://bit.ly/3aZVfAM',
+        frontImg: skullfront,
+        backImg: skullback,
+        price: '$23',
+    },
+    {
+        name: 'Classic (Wht)',
+        url: 'https://bit.ly/30ECv52',
+        frontImg: classicfront,
+        backImg: classicback,
+        price: '$18',
+    },
+    {
+        name: 'Classic (Blk)',
+        url: 'https://bit.ly/2ZPCegc',
+        frontImg: classicblkfront,
+        backImg: classicblkback,
+        price: '$18',
+    },
+    {
+        name: 'Basic',
+        url: 'https://bit.ly/3spoUdV',
+        frontImg: basic,
+        backImg: null,
+        price: '$18',
+    },
+    {
+        name: 'Rose Tank',
+        url: 'https://bit.ly/2QmdWVC',
+        frontImg: rosetank,
+        backImg: null,
+        price: '$15',
+    },
+]
+
+export default merchItems;
