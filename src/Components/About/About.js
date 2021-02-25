0996ae76da2f5c12aa391d3ac60bbb577e3dcd4d
monstaro <codysmith2814@gmail.com>
2021-02-25 10:15:53 -0700

    Add media query

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,30 +1,19 @@
 import React from "react";
 import "./About.scss";
 import aboutpic from "../../images/about-pic.jpg";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const About = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
+    <Container className="about-area">
+        <div className="img-container">
           <img
             className="about-img"
             src={aboutpic}
-            alt="the boys at the beach"
+            alt="hellocentral"
           />
-          <Container>
-            <Row>
-              <p>Frankie McGinn - Bass///</p>
-              <p>Cody Smith - Guitar, Vox</p>
-            </Row>
-            <Row>
-              <p>Rob Knox - Drums///</p>
-              <p>Justin Jokinen - Guitar</p>
-            </Row>
-          </Container>
-        </Col>
-        <Col sm>
+        </div>
+        <div className="about-area-text">
           Fueled by an ambitious, genre-spanning take on modern alternative
           rock, hellocentral has taken over the Denver music scene with their
           energetic and emotional live performances. Formed by four life-long
@@ -46,52 +35,10 @@ const About = () => {
           stations and music blogs. The band is currently working on their first
           full-length LP, said to infuse elements of trap and hip-hop into their
           ever-evolving sound. The LP is slated for release Autumn 2021.
-        </Col>
-      </Row>
+        </div>
     </Container>
   );
 };
 
-// const About = () => {
-//   return (
-
-//       {/* <div className="about-area">
-//         <div className="image-container">
-//           <img className="about-img" src={aboutpic} alt="the boys at the beach" />
-//           <div className="members">
-//             <p>Rob Knox - Drums</p>
-//             <p>Justin Jokinen - Guitar</p>
-//             <p>Cody Smith - Guitar, Vox</p>
-//             <p>Frank McGinn - Bass</p>
-//           </div>
-//         </div>
-//         <p className="about-area-text">
-//           Fueled by an ambitious, genre---spanning take on modern alternative
-//           rock, hellocentral has taken the Denver music scene by storm over the
-//           past three years with their energetic and emotional live performances.
-//           Formed by four life---long friends, hellocentral is the product of a
-//           culmination of influences, experiences, and a common passion for
-//           music. Compared to the likes of Brand New, Turnover, and Blink 182,
-//           hellocentral set out to create music that people can not only dance
-//           to, but also connect with. Blending dreamy indie rock elements with
-//           hard---hitting, catchy pop punk choruses, the group is turning heads
-//           with their unique and refreshing sound. The band released their
-//           self---titled, self- recorded debut EP in 2018, followed by a string
-//           of local shows that added a loyal base of fans along the way. The EP
-//           caught the attention of producers Glenn Sawyer and Rich Veltrop at The
-//           Spot Studios, where hellocentral recorded and independently released 4
-//           singles in 2019. Frequently playing shows to promote their growing
-//           catalogue, hellocentral is becoming a prominent figure in the
-//           alternative/punk markets in Denver, earning local press and larger
-//           stages. hellocentral’s next release “HEATWAVE” is slated to drop
-//           August 21, 2020. The 3 song EP looks to be a breath of fresh air for
-//           the alternative-rock scene, combining pop- punk, and indie/electronic
-//           elements to create powerhouse tracks with contagiously catchy melodies
-//           that fans of all genres can identify with.
-//         </p>
-//       </div> */}
-
-//   );
-// };
 
 export default About;
